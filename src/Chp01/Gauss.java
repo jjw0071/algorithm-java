57a04f76fed2555d9a1a36aabf4cb2d225ffb430
jjw0071 <jjw0071@naver.com>
2022-12-27 12:35:39 +0900

    Gauss sum

---
+++ src/Chp01/Gauss.java
@@ -0,0 +1,28 @@
+package Chp01;
+import java.util.Scanner;
+
+//가우스 덧셈을 구현한 클래스
+public class Gauss {
+    public static void start() {
+        int go;
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.println("1부터 n까지의 합을 가우스 덧셈으로 구합니다.\nn값을 입력하세요.");
+            System.out.println("n 값");
+            int n = sc.nextInt();
+            gausssum(n);
+            System.out.println("한번 더 하시겠습니까?(계속하시려면 1을 누르세요)");
+            go = sc.nextInt();
+        }while(go == 1);
+    }
+
+    public static void gausssum(int n){
+        int sum;
+        if(n%2 == 0){
+            sum = (1+n)*(n/2);
+        }else{
+            sum = (1+n)*(n/2)+((1+n)/2);
+        }
+        System.out.println("합은 "+sum+" 입니다.");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Chp01.Median.start();
+        Chp01.Gauss.start();
         //this is from home desktop
 
 
