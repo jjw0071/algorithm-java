a3e4da870f9da44e99fed56511e4df83b52c3106
jjw0071 <jjw0071@naver.com>
2023-01-10 16:36:18 +0900

    method counting prime number in (1~1000) added

--- src/Chp02/PrimeNumber1.java
+++ src/Chp02/PrimeNumber1.java
@@ -1,4 +1,21 @@
 package Chp02;
 
 public class PrimeNumber1 {
+    public static void start() {
+       //print all prime number in range of 1~1000
+        int counter = 0; //number of primenumber
+
+        for(int n=2 ;n<1000;n++){
+            int i;
+            for(i =2;i<n;i++){
+                if(n%i == 0)
+                    break;
+            }
+            if(n == i){
+                System.out.println(n);
+                counter ++;
+            }
+        }
+        System.out.println("1000까지 소수의 수 : "+counter);
+    }
 }
