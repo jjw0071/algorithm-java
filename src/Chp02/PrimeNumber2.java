41d618be82fd647d208a9c66ec663ea7334589fa
jjw0071 <jjw0071@naver.com>
2023-01-10 19:38:10 +0900

    Primenumber2 start method added

--- src/Chp02/PrimeNumber2.java
+++ src/Chp02/PrimeNumber2.java
@@ -1,4 +1,29 @@
 package Chp02;
 
 public class PrimeNumber2 {
+    public static void start() {
+        int counter = 0; // check how many times calculated
+        int ptr = 0;
+        int[] prime = new int[500];
+
+        prime[ptr++]=2;
+
+        for(int n = 3; n<= 1000;n+=2){
+            int i;
+            for (i =1 ;i<ptr;i++){
+                counter++;
+                if(n%prime[i]==0){
+                    break;
+                }
+            }
+            if(ptr == i)
+                prime[ptr++]=n;
+        }
+
+        for (int i =0;i<ptr;i++){
+            System.out.println(prime[i]);
+        }
+        System.out.println("나눗셈을 수행한 횟수 : "+counter);
+
+    }
 }
