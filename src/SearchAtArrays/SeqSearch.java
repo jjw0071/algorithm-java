ae0481a1eb1ba5f86b8f35b857aef0de3d589762
jjw0071 <jjw0071@naver.com>
2023-01-15 19:03:33 +0900

    seqSearch method added at SeqSearch class

--- src/SearchAtArrays/SeqSearch.java
+++ src/SearchAtArrays/SeqSearch.java
@@ -6,24 +6,35 @@ public class SeqSearch {
     public static void start() {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("요솟수 : ");
+        System.out.print("요솟수 : ");
         int num = sc.nextInt();
         int[] x = new int[num];
 
         for(int i = 0 ;i< num;i++){
-            System.out.println("x["+i+"]: ");
+            System.out.print("x["+i+"]: ");
             x[i] = sc.nextInt();
         }
 
-        System.out.println("검색할 값 : ");
+        System.out.print("검색할 값 : ");
         int ky = sc.nextInt();
 
         int idx = seqSearch(x, num, ky);
 
         if(idx == -1){
-            System.out.println("그런 요소가 없습니다.");
+            System.out.print("그런 요소가 없습니다.");
         }else{
-            System.out.println("그 값은 x["+idx+"]에 있습니다.");
+            System.out.print("그 값은 x["+idx+"]에 있습니다.");
+        }
+    }
+
+    static int seqSearch(int[] x, int num, int key){
+        int i = 0;
+        while(true){
+            if(i==num)
+                return -1;
+            if(x[i] == key)
+                return i;
+            i++;
         }
     }
 
