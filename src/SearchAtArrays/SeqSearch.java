d51e3fb52a40e59dc45d30b1cd8e370e740d19dc
jjw0071 <jjw0071@naver.com>
2023-01-15 18:59:33 +0900

    start method added at SeqSearch class

--- src/SearchAtArrays/SeqSearch.java
+++ src/SearchAtArrays/SeqSearch.java
@@ -1,5 +1,30 @@
 package SearchAtArrays;
 
+import java.util.Scanner;
+
 public class SeqSearch {
+    public static void start() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("요솟수 : ");
+        int num = sc.nextInt();
+        int[] x = new int[num];
+
+        for(int i = 0 ;i< num;i++){
+            System.out.println("x["+i+"]: ");
+            x[i] = sc.nextInt();
+        }
+
+        System.out.println("검색할 값 : ");
+        int ky = sc.nextInt();
+
+        int idx = seqSearch(x, num, ky);
+
+        if(idx == -1){
+            System.out.println("그런 요소가 없습니다.");
+        }else{
+            System.out.println("그 값은 x["+idx+"]에 있습니다.");
+        }
+    }
 
 }
