4c995cdbe61a856876695f6059445381e81498b0
jjw0071 <jjw0071@naver.com>
2023-02-05 19:50:57 +0900

    bj1927 done

--- src/bj1927/Main.java
+++ src/bj1927/Main.java
@@ -1,4 +1,25 @@
 package bj1927;
 
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int num = sc.nextInt();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int input;
+        for(int i = 0; i<num;i++){
+            input = sc.nextInt();
+            if (input == 0) {
+                if (pq.isEmpty())
+                    sb.append(0).append('\n');
+                else sb.append(pq.poll()).append('\n');
+            } else {
+                pq.add(input);
+            }
+        }
+        System.out.println(sb);
+    }
 }
