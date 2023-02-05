dd885a499b659b0a81972385bfd9c3391644bd75
jjw0071 <jjw0071@naver.com>
2023-02-05 20:19:17 +0900

    bj10866 done

---
+++ src/bj10866/Main.java
@@ -0,0 +1,55 @@
+package bj10866;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int num = sc.nextInt();
+        ArrayDeque<Integer> ad = new ArrayDeque<>();
+        String command;
+        int commandnum;
+        for(int i = 0 ; i < num ; i++){
+            command = sc.next();
+            switch (command) {
+                case "push_front" -> {
+                    commandnum = sc.nextInt();
+                    ad.addFirst(commandnum);
+                }
+                case "push_back" -> {
+                    commandnum = sc.nextInt();
+                    ad.addLast(commandnum);
+                }
+                case "pop_front" -> {
+                    if (ad.isEmpty())
+                        sb.append(-1).append('\n');
+                    else sb.append(ad.removeFirst()).append('\n');
+                }
+                case "pop_back" -> {
+                    if (ad.isEmpty())
+                        sb.append(-1).append('\n');
+                    else sb.append(ad.removeLast()).append('\n');
+                }
+                case "size" -> sb.append(ad.size()).append('\n');
+                case "empty" -> {
+                    if (ad.isEmpty())
+                        sb.append(1).append('\n');
+                    else sb.append(0).append('\n');
+                }
+                case "front" -> {
+                    if (ad.isEmpty())
+                        sb.append(-1).append('\n');
+                    else sb.append(ad.getFirst()).append('\n');
+                }
+                case "back" -> {
+                    if (ad.isEmpty())
+                        sb.append(-1).append('\n');
+                    else sb.append(ad.getLast()).append('\n');
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
