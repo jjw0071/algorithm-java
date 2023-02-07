225af0b1c115071a478b785a710bee66005e0c32
jjw0071 <jjw0071@naver.com>
2023-02-07 12:58:01 +0900

    bj11279 done

--- src/bj11279/Main.java
+++ src/bj11279/Main.java
@@ -1,6 +1,6 @@
 package bj11279;
 
-import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
@@ -9,25 +9,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
         int num = sc.nextInt();
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        ArrayDeque<Integer> ad = new ArrayDeque<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         int command;
-        int size;
         for(int i = 0; i<num;i++){
             command = sc.nextInt();
             if(command == 0){
                 if(pq.isEmpty())
                     sb.append(0).append('\n');
                 else{
-                    size = pq.size();
-                    for(int j = 0 ;j<size;j++){
-                        ad.addLast(pq.poll());
-                    }
-                    sb.append(ad.pollLast()).append('\n');
-                    size = ad.size();
-                    for(int j = 0 ; j<size;j++){
-                        pq.add(ad.pollLast());
-                    }
+                   sb.append(pq.poll()).append('\n');
                 }
 
             }
