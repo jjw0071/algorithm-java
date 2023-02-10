6c933ba8dd63f7debf205cfb98ada000b6760bd4
jjw0071 <jjw0071@naver.com>
2023-02-10 16:43:57 +0900

    pm 12909 done

---
+++ src/pm12909/Solution.java
@@ -0,0 +1,32 @@
+package pm12909;
+
+import java.util.Stack;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    boolean solution(String s) {
+        boolean answer = true;
+        char[] arr= s.toCharArray();
+        Stack<Character> st = new Stack<>();
+        char com;
+        for(int i = 0 ;i < arr.length;i++){
+            com = arr[i];
+            switch (com){
+                case '(' : st.add(com);
+                break;
+                case ')' :
+                    if(st.isEmpty()){
+                        return false;
+                    }
+                    st.pop();
+                    break;
+                }
+            }
+        return st.isEmpty();
+        }
+
+}
+
