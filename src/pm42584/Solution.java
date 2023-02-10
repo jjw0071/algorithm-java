b79c92d19740aba56cf9a2f0890374984c066968
jjw0071 <jjw0071@naver.com>
2023-02-10 16:14:41 +0900

    pm 42584 done

---
+++ src/pm42584/Solution.java
@@ -0,0 +1,28 @@
+package pm42584;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        int count;
+        for(int i = 0;i<prices.length;i++){
+            for(int j = i+1;j<prices.length;j++){
+                if(prices[i]>prices[j]){
+                    count = j - i;
+                    answer[i] = count;
+                    break;
+                }
+                if(j == prices.length -1){
+                    count = j - i;
+                    answer[i] = count;
+                    break;
+                }
+            }
+
+        }
+        return answer;
+    }
+}
