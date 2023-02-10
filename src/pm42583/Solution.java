073d13b4639741c4b4883b67c9888e493e4b6e5e
jjw0071 <jjw0071@naver.com>
2023-02-10 17:19:52 +0900

    pm 42583 added

---
+++ src/pm42583/Solution.java
@@ -0,0 +1,21 @@
+package pm42583;
+
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        int truck_after_bridge = 0;
+        ArrayDeque<Integer> ad = new ArrayDeque<>();
+        while(truck_after_bridge < truck_weights.length){
+
+        }
+        return answer;
+    }
+}
