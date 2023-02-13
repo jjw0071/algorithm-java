65e08926159b76d9e3003b02530bafb146091189
jjw0071 <jjw0071@naver.com>
2023-02-13 20:36:37 +0900

    bj 11053 done

--- src/bj11053/Main.java
+++ src/bj11053/Main.java
@@ -1,48 +1,41 @@
 package bj11053;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        int[][] howlong = new int[num][num+1];
+        int[] input = new int[num];
         for(int i = 0 ; i < num; i++){
-            howlong[i][0] = 0;
+            input[i] = sc.nextInt();
         }
-
-        for(int i = 0; i<num;i++){
-            int input = sc.nextInt();
-            boolean check = false;
-            int j = 0;
-            while(!check){
-                if(howlong[j][0] == 0){
-                    howlong[j][1] = input;
-                    howlong[j][0] = 1;
-                    check = true;
-                }else if(howlong[j][howlong[j][0]]<input){
-                    howlong[j][howlong[j][0]+1] = input;
-                    howlong[j][0]++;
-                    check = true;
-                }else {
-                    j++;
+        int[] check = new int[num];
+        int forComp;
+        for(int i = 0;i<input.length;i++){
+            forComp = input[i];
+            int max = 1;
+            boolean thereisbig = false;
+            if(i != 0){
+                for(int j = 0 ; j < i;j++){
+                    if(input[j]<forComp){
+                        thereisbig = true;
+                        if(max<check[j])
+                            max = check[j];
+                    }
                 }
+                if(thereisbig)
+                    max++;
             }
+            check[i] = max;
+        }
+        int max = 1;
+        for(int i = 0 ; i < check.length;i++){
+            if(check[i]>max)
+                max = check[i];
         }
 
-        for(int i = 0; i<howlong.length;i++){
-            for(int j = 0;j<howlong[0].length;j++)
-                System.out.print(howlong[i][j]+" ");
-            System.out.println();
-        }
-
-        int answer = 0;
-        for(int i = 0; i<howlong.length;i++){
-            if(howlong[i][0]>answer)
-                answer = howlong[i][0];
-        }
-        if(answer == 1)
-            answer = 0;
-        System.out.println(answer);
+        System.out.println(max);
     }
 }
