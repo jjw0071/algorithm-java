d0cccbb478e8bd8a8bcfbf2697ddfec5ee517796
jjw0071 <jjw0071@naver.com>
2023-03-01 21:45:03 +0900

    pm43165 done

---
+++ src/Programmers/pm43165_타겟넘버.java
@@ -0,0 +1,48 @@
+package Programmers;
+
+public class pm43165_타겟넘버 {
+    static int solution(int[] numbers, int target) {
+        int[] checked = new int[numbers.length];
+        int end = 0;
+        for(int i = 0 ; i<numbers.length;i++){
+            end -= numbers[i];
+        }
+        int compare = 0;
+        int answer = 0;
+        boolean check;
+        while(compare!=end){
+            check = true;
+            compare = 0;
+            for(int i = 0 ; i<numbers.length;i++){
+                if(checked[i] == 2){
+                    checked[i-1]++;
+                    checked[i] = 0;
+                    i -=2;
+                }
+            }
+            for(int i = 0 ; i<numbers.length;i++){
+                if(checked[i] == 0){
+                    compare += numbers[i];
+                }
+                if(checked[i] == 1){
+                    compare -= numbers[i];
+                }
+                if(i == numbers.length-1){
+                    checked[i]++;
+                }
+            }
+            if(check && compare == target){
+                System.out.println(" Correct!");
+                answer++;
+            }
+
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = new int[]{1,1,1,1};
+        int target = 3;
+        solution(numbers, target);
+    }
+}
