5e9f4f8cc0efaa2064b34fb16fbba2dce4356f37
jjw0071 <jjw0071@naver.com>
2023-03-03 11:22:29 +0900

    pm42842_카펫 done

---
+++ src/Programmers/pm42842_카펫.java
@@ -0,0 +1,25 @@
+package Programmers;
+
+public class pm42842_카펫 {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int total = brown+yellow;
+        int length = 1;
+        int height = 0;
+        boolean correct = true;
+        while(correct){
+            if(total%length != 0){
+                length++;
+                continue;
+            }
+            height = total/length;
+            if(brown == (height+length)*2-4){
+                break;
+            }
+            length++;
+        }
+        answer[0] = height;
+        answer[1] = length;
+        return answer;
+    }
+}
