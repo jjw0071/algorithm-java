185c6b70463ef57998565e3f8987c0b83d4d6280
jjw0071 <jjw0071@naver.com>
2023-03-03 12:06:09 +0900

    pm86971_전력망둘로나누기 added

---
+++ src/Programmers/pm86971_전력망둘로나누기.java
@@ -0,0 +1,11 @@
+package Programmers;
+
+public class pm86971_전력망둘로나누기 {
+    public int solution(int n, int[][] wires) {
+        int[] visited = new int[n+1];
+        int answer = 0;
+
+
+        return answer;
+    }
+}
