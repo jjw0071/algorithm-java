12d6a2cc4e4033bb41a135d58612fec6cb808d3c
jjw0071 <jjw0071@naver.com>
2023-03-10 15:26:13 +0900

    bj2839_설탕배달 added

---
+++ src/Baekjoon/bj2839_설탕배달.java
@@ -0,0 +1,19 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class bj2839_설탕배달 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int answer = -1 ;
+        int amount = sc.nextInt();
+        int amount2 = amount;
+        int temp;
+
+
+
+        System.out.println(answer);
+    }
+
+
+}
