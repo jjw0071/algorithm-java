40d52f174f0589b6316a53bc53f80b72a62e7391
jjw0071 <jjw0071@naver.com>
2023-03-23 21:39:00 +0900

    lt589N_aryTreePreorderTraversal 주석처리

--- src/LeetCode/lt589N_aryTreePreorderTraversal.java
+++ src/LeetCode/lt589N_aryTreePreorderTraversal.java
@@ -3,23 +3,24 @@ package LeetCode;
 import java.util.ArrayList;
 import java.util.List;
 
+// 문제는 맞췄지만 다른 곳에서 빌드하면 여기서 에러나서 주석처리함.
 public class lt589N_aryTreePreorderTraversal {
-    public List<Integer> preorder(Node root) {
-        List<Integer> answer = new ArrayList<>();
-        check(answer,root);
-        return answer;
-    }
-
-    public static Node check(List<Integer> answer, Node root){
-        if(root == null){
-            return null;
-        }
-        answer.add(root.val);
-        if(root.children != null){
-            for(int i = 0 ; i < root.children.size();i++){
-                check(answer, root.children.get(i));
-            }
-        }
-        return null;
-    }
+//    public List<Integer> preorder(Node root) {
+//        List<Integer> answer = new ArrayList<>();
+//        check(answer,root);
+//        return answer;
+//    }
+//
+//    public static Node check(List<Integer> answer, Node root){
+//        if(root == null){
+//            return null;
+//        }
+//        answer.add(root.val);
+//        if(root.children != null){
+//            for(int i = 0 ; i < root.children.size();i++){
+//                check(answer, root.children.get(i));
+//            }
+//        }
+//        return null;
+//    }
 }
