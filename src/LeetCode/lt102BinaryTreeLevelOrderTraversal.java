0b12abec17907d5bc903b0bd88d0ccee55459637
jjw0071 <jjw0071@naver.com>
2023-03-23 21:39:27 +0900

    lt102BinaryTreeLevelOrderTraversal done, 주석처리

---
+++ src/LeetCode/lt102BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,33 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 문제는 맞췄지만 다른 곳에서 빌드하면 여기서 에러나서 주석처리함.
+
+public class lt102BinaryTreeLevelOrderTraversal {
+//    public List<List<Integer>> levelOrder(TreeNode root) {
+//        List<List<Integer>> answer = new ArrayList<>();
+//        check(answer, root, 0);
+//        return answer;
+//
+//    }
+//
+//    public static TreeNode check(List<List<Integer>> answer, TreeNode root, int depth){
+//        if(root == null){
+//            return null;
+//        }
+//        if(answer.size()<depth || answer.size() == depth){
+//            answer.add(new ArrayList<>());
+//        }
+//        answer.get(depth).add(root.val);
+//        if(root.left != null){
+//            check(answer, root.left, depth+1);
+//        }
+//        if(root.right != null){
+//            check(answer, root.right, depth+1);
+//        }
+//        return null;
+//
+//    }
+}
