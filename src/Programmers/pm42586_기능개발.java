8b16255d526a0a6592c0b43bbd30703898236f2a
jjw0071 <jjw0071@naver.com>
2023-06-01 20:18:07 +0900

    pm42586_기능개발 added

---
+++ src/Programmers/pm42586_기능개발.java
@@ -0,0 +1,12 @@
+package Programmers;
+
+public class pm42586_기능개발 {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        return answer;
+    }
+}
