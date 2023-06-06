c4575a2951bfd7ad8089c2781deb1a74a662a49a
jjw0071 <jjw0071@naver.com>
2023-06-06 17:22:53 +0900

    pm42586_기능개발 done

--- src/Programmers/pm42586_기능개발.java
+++ src/Programmers/pm42586_기능개발.java
@@ -8,37 +8,49 @@ public class pm42586_기능개발 {
     }
 
     public int[] solution(int[] progresses, int[] speeds) {
-        Stack<Integer> st = new Stack<>();
         Stack<Integer> ans = new Stack<>();
-        
-        int temp =0;
-        int days = 0;
-        int totaldays = 0;
-        daystogo(progresses, speeds, 0);
-        st.add(days);
-        totaldays += days;
-        for(int i = 1 ; i< speeds.length; i++){
-            daystogo(progresses, speeds, i);
-            temp=st.peek();
-            if(temp>days){
-                st.add(days);
-            }else if(temp == days){
-                ans.add(st.size()+1);
-            }else {
-                ans.add(st.size());
-                st.clear();
-                st.add(days);
+
+        int togo = 1;
+        int days = check(progresses[0],speeds[0]);
+        int temp;
+        for(int i = 1; i < progresses.length;i++){
+            System.out.println(i);
+            temp=check(progresses[i],speeds[i]);
+            if(days == 0){
+                days = temp;
+                continue;
             }
-            
+            if(temp>days){
+                System.out.println(togo+" into answ");
+                ans.add(togo);
+                togo = 1;
+                days = temp;
+            }else {
+                System.out.println("togo++");
+                togo++;
+            }
+
+        }
+        System.out.println(togo);
+        if(togo != 0){
+            ans.add(togo);
+        }
+        System.out.println(ans.size());
+        int[] answer = new int[ans.size()];
+        for(int i = ans.size()-1;i>-1;i--){
+            answer[i] = ans.pop();
         }
         return answer;
+
     }
 
-    private static void daystogo(int[] progresses, int[] speeds, int i) {
-        int days;
-        days = (100- progresses[i])/ speeds[i];
-        if((100- progresses[i])% speeds[i] != 0){
-            days++;
+    public static int check(int pro, int speeds){
+        int cal = (100-pro)/speeds;
+        if((100-pro)%speeds == 0){
+            return cal;
         }
+        return cal+1;
+
     }
 }
+
