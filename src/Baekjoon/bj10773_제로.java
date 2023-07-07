17f3371fdf66a397575fc9de234061c126bdfa7c
jjw0071 <jjw0071@naver.com>
2023-07-07 17:28:33 +0900

    bj10773_제로 done

---
+++ src/Baekjoon/bj10773_제로.java
@@ -0,0 +1,30 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class bj10773_제로 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        int input;
+        for(int i = 0 ; i< count;i++){
+            input = Integer.parseInt(br.readLine());
+            if(input == 0){
+                stack.pop();
+            }else{
+                stack.push(input);
+            }
+        }
+        int sum = 0 ;
+        while(!stack.isEmpty()){
+            sum += stack.pop();
+        }
+
+        System.out.println(sum);
+
+    }
+}
