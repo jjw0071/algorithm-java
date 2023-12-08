4a88688cdd5d7bde08c001e163efa7158ea59a85
jjw0071 <jjw0071@naver.com>
2023-12-08 15:24:56 +0900

    프로그래머스 template 패키지 생성

---
+++ src/Programmers/A_template_package/Solution.java
@@ -0,0 +1,11 @@
+package Programmers.A_template_package;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static String solution(){
+        return "abc";
+    }
+}
