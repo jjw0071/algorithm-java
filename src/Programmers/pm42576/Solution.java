866fc9f87fc1819060126e488aff75662107dfc2
jjw0071 <jjw0071@naver.com>
2023-12-08 15:24:21 +0900

    pm42576 문제 해결

---
+++ src/Programmers/pm42576/Solution.java
@@ -0,0 +1,44 @@
+package Programmers.pm42576;
+
+import java.util.HashMap;
+
+public class Solution {
+    public static void main(String[] args) {
+        String[] participant = new String[] {
+                "leo", "kiki", "eden"
+        };
+
+        String[] completion = new String[]{
+                "eden", "kiki"
+        };
+
+        System.out.println(solution(participant,completion));
+    }
+    public static String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> complete = new HashMap<>();
+        int oldCount =0;
+        for(String player : completion){
+            if(complete.containsKey(player)){
+                oldCount = complete.get(player);
+                oldCount++;
+                complete.replace(player,oldCount);
+            }else{
+                complete.put(player,1);
+            }
+        }
+
+        for(String player : participant){
+            if(!complete.containsKey(player)){
+                return player;
+            }
+            if(complete.get(player) == 0){
+                return player;
+            }else{
+                oldCount = complete.get(player);
+                oldCount--;
+                complete.replace(player,oldCount);
+            }
+        }
+        return null;
+    }
+}
