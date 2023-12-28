6518d6ac1de4027e18952dd4bf02a617784e0f5e
jjw0071 <jjw0071@naver.com>
2023-12-28 15:20:20 +0900

    pm42627_디스크컨트롤러 해결

---
+++ src/Programmers/pm42627_디스크컨트롤러/Solution.java
@@ -0,0 +1,11 @@
+package Programmers.pm42627_디스크컨트롤러;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static String solution(){
+        return "abc";
+    }
+}
