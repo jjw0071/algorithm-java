89e643c3dba1a64751dffbf58d93ae6bff81f633
jjw0071 <jjw0071@naver.com>
2024-01-25 18:02:50 +0900

    SWEA template 생성

---
+++ src/SWEA/A_template_package/Solution.java
@@ -0,0 +1,25 @@
+package SWEA.A_template_package;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Solution{
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        int answer = 0;
+
+        for(int test_case = 1; test_case <= T; test_case++){
+
+
+        }
+
+
+
+        bw.write(answer+"\n");
+        bw.flush();
+        bw.close();
+    }
+}
