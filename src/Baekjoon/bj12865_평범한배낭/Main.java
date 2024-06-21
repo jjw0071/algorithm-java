ed922e99a48387f65f0ba480ad162efb2a11c4e6
jjw0071 <jjw0071@naver.com>
2024-06-21 10:02:33 +0900

    bj12865_평범한배낭 해결
    dp
    knapsack problem

---
+++ src/Baekjoon/bj12865_평범한배낭/Main.java
@@ -0,0 +1,39 @@
+package Baekjoon.bj12865_평범한배낭;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class    Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] weights = new int[K+1];
+        int[] best = new int[K+1];
+
+        int[][] item = new int[N + 1][2];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            item[i][0] = Integer.parseInt(st.nextToken());
+            item[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        int[][] dp = new int[N + 1][K + 1];
+
+        for (int k = 1; k <= K; k++) { // 무게
+            for (int i = 1; i <= N; i++) { // item
+                dp[i][k] = dp[i - 1][k];
+                if (k - item[i][0] >= 0) {
+                    dp[i][k] = Math.max(dp[i - 1][k], item[i][1] + dp[i - 1][k - item[i][0]]);
+                }
+            }
+        }
+
+        bw.write(dp[N][K]+"\n");
+        bw.flush();
+        bw.close();
+    }
+}
